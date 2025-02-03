2e20310737525e1dcfaba0c3d4c61d5225c454cd
Camopoltergeist <camopoltergeist@outlook.com>
2025-02-03 14:12:10 +0200

    Implemented Bishop struct

---
+++ src/pieces/bishop.rs
@@ -0,0 +1,37 @@
+use crate::{board::{bitboard::Bitboard, move_mask::BISHOP_MASKS, tile_position::TilePosition}, piece::{Piece, PieceType}, player::Player};
+
+pub struct Bishop {
+    player: Player,
+    tile_position: TilePosition,
+}
+
+impl Bishop {
+    pub fn new(player: Player, tile_position: TilePosition) -> Self {
+        Self {
+            player,
+            tile_position
+        }
+    }
+
+    pub const fn get_movement_mask(tile_position: TilePosition) -> Bitboard {
+        BISHOP_MASKS[tile_position.bit_offset() as usize]
+    }
+}
+
+impl Piece for Bishop {
+    fn piece_type(&self) -> PieceType {
+        PieceType::Bishop
+    }
+
+    fn player(&self) -> Player {
+        self.player
+    }
+
+    fn tile_position(&self) -> TilePosition {
+        self.tile_position
+    }
+
+    fn movement_mask(&self) -> Bitboard {
+        Self::get_movement_mask(self.tile_position)
+    }
+}
