4e7c48ecca7d5df6026ba232b0a7215e5dd89fc6
Camopoltergeist <camopoltergeist@outlook.com>
2025-02-03 14:46:30 +0200

    Moved QUEEN_MASKS into Queen struct

--- src/board/move_mask.rs
+++ src/board/move_mask.rs
@@ -1,11 +1,10 @@
-use crate::{board::tile_position::TilePosition, pieces::queen::Queen};
+use crate::board::tile_position::TilePosition;
 
 use super::bitboard::Bitboard;
 
 use const_for::const_for;
 
 pub const KING_MASKS: [Bitboard; 64] = generate_king_masks();
-pub const QUEEN_MASKS: [Bitboard; 64] = Queen::generate_all_movement_masks();
 pub const WHITE_PAWN_MASKS: [Bitboard; 64] = generate_white_pawn_masks();
 pub const BLACK_PAWN_MASKS: [Bitboard; 64] = generate_black_pawn_masks();
 
--- src/pieces/queen.rs
+++ src/pieces/queen.rs
@@ -1,4 +1,4 @@
-use crate::{board::{bitboard::Bitboard, move_mask::QUEEN_MASKS, tile_position::TilePosition}, piece::{Piece, PieceType}, player::Player};
+use crate::{board::{bitboard::Bitboard, tile_position::TilePosition}, piece::{Piece, PieceType}, player::Player};
 
 use super::{bishop::Bishop, rook::Rook};
 
@@ -10,6 +10,8 @@ pub struct Queen {
 }
 
 impl Queen {
+    pub const MOVEMENT_MASKS: [Bitboard; 64] = Self::generate_all_movement_masks();
+
     pub fn new(player: Player, tile_position: TilePosition) -> Self {
         Self {
             player,
@@ -18,7 +20,7 @@ impl Queen {
     }
 
     pub const fn get_movement_mask(tile_position: TilePosition) -> Bitboard {
-        QUEEN_MASKS[tile_position.bit_offset() as usize]
+        Self::MOVEMENT_MASKS[tile_position.bit_offset() as usize]
     }
 
     pub const fn generate_movement_mask(tile_position: TilePosition) -> Bitboard {
