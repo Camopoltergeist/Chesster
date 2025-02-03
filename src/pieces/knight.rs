b75559155bf8acb3cf1bac52a38136ffc10318bc
Camopoltergeist <camopoltergeist@outlook.com>
2025-02-03 13:25:12 +0200

    Implemented Knight piece

---
+++ src/pieces/knight.rs
@@ -0,0 +1,37 @@
+use crate::{board::{bitboard::Bitboard, move_mask::KNIGHT_MASKS, tile_position::TilePosition}, piece::{Piece, PieceType}, player::Player};
+
+pub struct Knight {
+    player: Player,
+    tile_position: TilePosition
+}
+
+impl Knight {
+    pub fn new(player: Player, tile_position: TilePosition) -> Self {
+        Self {
+            player,
+            tile_position
+        }
+    }
+
+    pub fn get_movement_mask(tile_position: TilePosition) -> Bitboard {
+        KNIGHT_MASKS[tile_position.bit_offset() as usize]
+    }
+}
+
+impl Piece for Knight {
+    fn piece_type(&self) -> PieceType {
+        PieceType::Knight
+    }
+
+    fn player(&self) -> Player {
+        self.player
+    }
+
+    fn tile_position(&self) -> TilePosition {
+        self.tile_position
+    }
+
+    fn movement_mask(&self) -> Bitboard {
+        Self::get_movement_mask(self.tile_position)
+    }
+}
