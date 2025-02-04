a9ebba71d2b42848342ba8dac7ab7f33167dbc9b
ssntr <142799815+ssntr@users.noreply.github.com>
2025-02-04 12:47:21 +0200

    debugged and auto-formatted

--- src/board/attack_mask.rs
+++ src/board/attack_mask.rs
@@ -1,10 +1,17 @@
 use crate::piece::Piece;
 
-use super::{bitboard::Bitboard, move_collision::{get_collision_mask, get_pawn_capture}, position::Position, tile_position::TilePosition};
+use super::{
+    bitboard::Bitboard,
+    move_collision::{get_collision_mask, get_pawn_capture},
+    position::Position,
+    tile_position::TilePosition,
+};
 
 ///Returns a bitboard of every tile the opposite player can move to on their next turn
 pub fn get_opposite_attack_mask(position: Position) -> Bitboard {
-    let enemy_board: Bitboard = *position.board().get_player_bitboard(position.current_player().opposite().clone());
+    let enemy_board: Bitboard = *position
+        .board()
+        .get_player_bitboard(position.current_player().opposite().clone());
 
     let mut attack_mask: Bitboard = Bitboard(0);
     for bit_offset in 0..64 {
@@ -18,7 +25,7 @@ pub fn get_opposite_attack_mask(position: Position) -> Bitboard {
                 Piece::Pawn => Bitboard(get_pawn_capture(player_piece.player(), tile_pos)),
                 _ => get_collision_mask(position.board().clone(), tile_pos),
             };
-            }
+        }
     }
 
     attack_mask
