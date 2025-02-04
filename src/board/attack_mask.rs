d17b3af42d7fcf8aa5cdc68f33f1db54e0018b4c
ssntr <142799815+ssntr@users.noreply.github.com>
2025-02-04 11:34:50 +0200

    added a seperate handling for pawns

--- src/board/attack_mask.rs
+++ src/board/attack_mask.rs
@@ -1,4 +1,6 @@
-use super::{bitboard::Bitboard, move_collision::get_collision_mask, position::Position, tile_position::TilePosition};
+use crate::piece::Piece;
+
+use super::{bitboard::Bitboard, move_collision::{get_collision_mask, get_pawn_capture}, position::Position, tile_position::TilePosition};
 
 ///Returns a bitboard of every tile the opposite player can move to on their next turn
 pub fn get_opposite_attack_mask(position: Position) -> Bitboard {
@@ -11,8 +13,11 @@ pub fn get_opposite_attack_mask(position: Position) -> Bitboard {
         }
 
         let tile_pos = TilePosition::from_bit_offset(bit_offset);
-        if let Some(_) = position.get_piece(tile_pos) {
-            attack_mask |= get_collision_mask(position.board().clone(), tile_pos);
+        if let Some(player_piece) = position.get_piece(tile_pos) {
+            attack_mask |= match player_piece.piece() {
+                Piece::Pawn => Bitboard(get_pawn_capture(player_piece.player(), tile_pos)),
+                _ => get_collision_mask(position.board().clone(), tile_pos),
+            };
             }
     }
 
