2cd79e89b3a2824b2e1c03abfd0abd592046e03a
Camopoltergeist <camopoltergeist@outlook.com>
2025-02-06 01:16:01 +0200

    Added debug assertions to TilePosition to check that positions are within the board

--- src/board/tile_position.rs
+++ src/board/tile_position.rs
@@ -6,6 +6,9 @@ pub struct TilePosition {
 
 impl TilePosition {
     pub const fn new(column: u32, rank: u32) -> Self {
+        debug_assert!(column <= 7);
+        debug_assert!(rank <= 7);
+
         Self {
             column,
             rank
