ece84e01375173cbc564acff1d0fc794f9e93a2d
Camopoltergeist <camopoltergeist@outlook.com>
2025-02-07 00:24:17 +0200

    Implemented material search

--- src/executor.rs
+++ src/executor.rs
@@ -1,5 +1,11 @@
-use crate::board::{board::Board, moove::Move};
+use crate::board::{moove::Move, position::Position};
 
-pub type EvaluationFn = fn(&Board) -> f32;
-pub type SearchFn = fn(&Board) -> Move;
+pub type EvaluationFn = fn(&Position) -> f32;
+pub type SearchFn = fn(&Position, EvaluationFn) -> Move;
 
+pub fn evaluate_material_only(position: &Position) -> f32 {
+	let own_material = position.board().get_material_for_for_player(position.current_player()) as f32;
+	let opponent_material = position.board().get_material_for_for_player(position.current_player().opposite()) as f32;
+
+	own_material - opponent_material
+}
