3f7215f7e0a7d72ed30c152ca04771116009fb86
Camopoltergeist <camopoltergeist@outlook.com>
2025-02-08 01:20:39 +0200

    Implemented multithreaded negamax

--- src/bot/search_funcs.rs
+++ src/bot/search_funcs.rs
@@ -1,3 +1,5 @@
+use std::thread;
+
 use crate::{board::{moove::Move, position::Position}, bot::evaluation::Evaluation};
 
 use super::EvaluationFn;
@@ -124,4 +126,81 @@ pub fn print_move_chain(move_chain: &Vec<Move>, evaluation: Evaluation) {
 	}
 
 	println!("{:?}", evaluation);
+}
+
+pub fn negamax_with_move_chain_multithreaded(position: &Position, evaluation_fn: EvaluationFn, depth: u32) -> (Evaluation, Vec<Move>) {
+	fn negamax(position: &Position, evaluation_fn: EvaluationFn, depth: u32) -> (Evaluation, Vec<Move>) {
+		if depth < 1 {
+			return (evaluation_fn(position), Vec::new());
+		};
+
+		let legal_moves = position.get_all_legal_moves();
+
+		if legal_moves.len() < 1 {
+			return (evaluation_fn(position), Vec::new());
+		}
+
+		let mut best_eval = Evaluation::Initial;
+		let mut best_move_chain = Vec::new();
+
+		for m in legal_moves {
+			let mut moved_position = position.clone();
+			moved_position.make_move(m.clone()).unwrap();
+
+			let (mut eval, mut eval_move_chain) = negamax(&moved_position, evaluation_fn, depth - 1);
+			eval = -eval;
+
+			eval_move_chain.push(m);
+
+			if eval > best_eval {
+				best_eval = eval;
+				best_move_chain = eval_move_chain;
+			}
+		};
+
+		return (best_eval, best_move_chain);
+	}
+
+	if depth < 1 {
+		return (evaluation_fn(position), Vec::new());
+	};
+
+	let legal_moves = position.get_all_legal_moves();
+
+	if legal_moves.len() < 1 {
+		return (evaluation_fn(position), Vec::new());
+	};
+
+	let mut best_eval = Evaluation::Initial;
+	let mut best_move_chain = Vec::new();
+
+	let mut threads = Vec::new();
+
+	for m in legal_moves {
+		let mut moved_position = position.clone();
+		moved_position.make_move(m.clone()).unwrap();
+
+		threads.push(thread::spawn(move || {
+
+			let (mut eval, mut eval_move_chain) = negamax(&moved_position, evaluation_fn, depth - 1);
+			eval = -eval;
+
+			eval_move_chain.push(m.clone());
+
+			return (eval, eval_move_chain);
+		}));
+	};
+
+	for t in threads {
+		let (eval, eval_move_chain) = t.join().unwrap();
+
+		print_move_chain(&eval_move_chain, eval.clone());
+
+		if eval > best_eval {
+			best_eval = eval.clone();
+			best_move_chain = eval_move_chain;
+		}
+	}
+
+	return (best_eval, best_move_chain);
 }
