2bf8f7ff271ec5c519bf69c6bb1c493b5808352e
Camopoltergeist <camopoltergeist@outlook.com>
2025-02-07 23:22:57 +0200

    Implement negamax with move chain

--- src/bot/search_funcs.rs
+++ src/bot/search_funcs.rs
@@ -55,3 +55,77 @@ pub fn negamax_search(position: &Position, evaluation_fn: EvaluationFn, depth: u
 
 	return (best_move.unwrap(), max);
 }
+
+pub fn negamax_with_move_chain(position: &Position, evaluation_fn: EvaluationFn, depth: u32) -> (Evaluation, Vec<Move>) {
+	fn negamax(position: &Position, evaluation_fn: EvaluationFn, depth: u32) -> (Evaluation, Vec<Move>) {
+		if depth < 1 {
+			return (evaluation_fn(position), Vec::new());
+		};
+
+		let legal_moves = position.get_all_legal_moves();
+
+		if legal_moves.len() < 1 {
+			return (evaluation_fn(position), Vec::new());
+		}
+
+		let mut best_eval = Evaluation::Checkmate(i32::MIN);
+		let mut best_move_chain = Vec::new();
+
+		for m in legal_moves {
+			let mut moved_position = position.clone();
+			moved_position.make_move(m.clone()).unwrap();
+
+			let (mut eval, mut eval_move_chain) = negamax(&moved_position, evaluation_fn, depth - 1);
+			eval = -eval;
+
+			eval_move_chain.push(m);
+
+			if eval > best_eval {
+				best_eval = eval;
+				best_move_chain = eval_move_chain;
+			}
+		};
+
+		return (best_eval, best_move_chain);
+	}
+
+	if depth < 1 {
+		return (evaluation_fn(position), Vec::new());
+	};
+
+	let legal_moves = position.get_all_legal_moves();
+
+	if legal_moves.len() < 1 {
+		return (evaluation_fn(position), Vec::new());
+	};
+
+	let mut best_eval = Evaluation::Checkmate(i32::MIN);
+	let mut best_move_chain = Vec::new();
+
+	for m in legal_moves {
+		let mut moved_position = position.clone();
+		moved_position.make_move(m.clone()).unwrap();
+
+		let (mut eval, mut eval_move_chain) = negamax(&moved_position, evaluation_fn, depth - 1);
+		eval = -eval;
+		
+		eval_move_chain.push(m);
+
+		print_move_chain(&eval_move_chain, eval.clone());
+
+		if eval > best_eval {
+			best_eval = eval.clone();
+			best_move_chain = eval_move_chain;
+		}
+	};
+
+	return (best_eval, best_move_chain);
+}
+
+pub fn print_move_chain(move_chain: &Vec<Move>, evaluation: Evaluation) {
+	for m in move_chain.iter().rev() {
+		print!("{} | ", m.debug_string());
+	}
+
+	println!("{:?}", evaluation);
+}
