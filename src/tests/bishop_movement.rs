58b28fd34ea1c4ddc7a9ef9549c850a76881ea68
Camopoltergeist <camopoltergeist@outlook.com>
2025-02-11 13:30:40 +0200

    Add bishop collisiont with opponent pieces test

--- src/tests/bishop_movement.rs
+++ src/tests/bishop_movement.rs
@@ -62,3 +62,30 @@ fn bishop_collision_with_friendly_pieces() {
 
     assert!(compare_moves(&desired_moves, &received_moves));
 }
+
+#[test]
+fn bishop_collision_with_opponent_pieces() {
+    let position = Position::from_fen_str("8/6p1/5p2/2p5/3B4/8/1p6/6p1 w - - 0 1").unwrap();
+
+    let desired_moves = vec![
+        // North West
+        Move::debug_new_basic("d4", "c5"),
+
+        // North East
+        Move::debug_new_basic("d4", "c3"),
+        Move::debug_new_basic("d4", "b2"),
+
+        // South East
+        Move::debug_new_basic("d4", "e3"),
+        Move::debug_new_basic("d4", "f2"),
+        Move::debug_new_basic("d4", "g1"),
+
+        // South West
+        Move::debug_new_basic("d4", "e5"),
+        Move::debug_new_basic("d4", "f6"),
+    ];
+
+    let received_moves = position.get_legal_moves_for_tile_position(TilePosition::from_tile_str("d4").unwrap());
+
+    assert!(compare_moves(&desired_moves, &received_moves));
+}
