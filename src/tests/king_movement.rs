91b1bfdab338b81ca766fc18e20e2049d2030a30
Camopoltergeist <camopoltergeist@outlook.com>
2025-02-11 14:30:17 +0200

    Add king collision with friendly pieces

--- src/tests/king_movement.rs
+++ src/tests/king_movement.rs
@@ -49,3 +49,18 @@ fn king_movement_without_collision_d4() {
 
     assert!(compare_moves(&desired_moves, &received_moves)); 
 }
+
+#[test]
+fn king_collision_with_friendly_pieces() {
+    let position = Position::from_fen_str("8/8/8/2P1P3/2PKP3/2P5/8/8 w - - 0 1").unwrap();
+
+    let desired_moves = vec![
+        Move::debug_new_basic("d4", "d5"),
+        Move::debug_new_basic("d4", "e3"),
+        Move::debug_new_basic("d4", "d3"),
+    ];
+
+    let received_moves = position.get_legal_moves_for_tile_position(TilePosition::from_tile_str("d4").unwrap());
+
+    assert!(compare_moves(&desired_moves, &received_moves)); 
+}
