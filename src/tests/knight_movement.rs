0d5401c7510b1e6f662ed800d5727b14d974d7b8
Camopoltergeist <camopoltergeist@outlook.com>
2025-02-11 13:51:59 +0200

    Add knight movement without collision h8

--- src/tests/knight_movement.rs
+++ src/tests/knight_movement.rs
@@ -1,10 +1,10 @@
 use crate::board::position::Position;
 use crate::board::{moove::Move, tile_position::TilePosition};
 
+use crate::tests::compare_moves;
+
 #[test]
 fn knight_movement_without_collision_a1() {
-    use crate::tests::compare_moves;
-
     let position = Position::from_fen_str("8/8/8/8/8/8/8/N7 w - - 0 1").unwrap();
 
     let desired_moves = vec![
@@ -15,4 +15,18 @@ fn knight_movement_without_collision_a1() {
     let received_moves = position.get_legal_moves_for_tile_position(TilePosition::from_tile_str("a1").unwrap());
 
     assert!(compare_moves(&desired_moves, &received_moves));
+}
+
+#[test]
+fn knight_movement_without_collision_h8() {
+    let position = Position::from_fen_str("7N/8/8/8/8/8/8/8 w - - 0 1").unwrap();
+
+    let desired_moves = vec![
+        Move::debug_new_basic("h8", "f7"),
+        Move::debug_new_basic("h8", "g6"),
+    ];
+
+    let received_moves = position.get_legal_moves_for_tile_position(TilePosition::from_tile_str("h8").unwrap());
+
+    assert!(compare_moves(&desired_moves, &received_moves));    
 }
