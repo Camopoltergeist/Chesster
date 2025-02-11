b1790c0d95c226e370dcf86f680a0192990c0f8b
Camopoltergeist <camopoltergeist@outlook.com>
2025-02-11 14:38:22 +0200

    Add king movement blocked by threatened tiles test

--- src/tests/king_movement.rs
+++ src/tests/king_movement.rs
@@ -84,3 +84,18 @@ fn king_collision_with_opponent_pieces() {
 
     assert!(compare_moves(&desired_moves, &received_moves));
 }
+
+#[test]
+fn king_movement_blocked_by_threatened_tiles() {
+    let position = Position::from_fen_str("4r3/8/8/7r/3K4/8/8/8 w - - 0 1").unwrap();
+
+    let desired_moves = vec![
+        Move::debug_new_basic("d4", "c4"),
+        Move::debug_new_basic("d4", "c3"),
+        Move::debug_new_basic("d4", "d3"),
+    ];
+
+    let received_moves = position.get_legal_moves_for_tile_position(TilePosition::from_tile_str("d4").unwrap());
+
+    assert!(compare_moves(&desired_moves, &received_moves));
+}
