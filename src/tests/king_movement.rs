1cb0d517450f2342e5001dddb84409d142d20a12
Camopoltergeist <camopoltergeist@outlook.com>
2025-02-11 14:17:32 +0200

    Add king movement without collision h8 test

--- src/tests/king_movement.rs
+++ src/tests/king_movement.rs
@@ -14,3 +14,18 @@ fn king_movement_without_collision_a1() {
 
     assert!(compare_moves(&desired_moves, &received_moves)); 
 }
+
+#[test]
+fn king_movement_without_collision_h8() {
+    let position = Position::from_fen_str("7K/8/8/8/8/8/8/8 w - - 0 1").unwrap();
+
+    let desired_moves = vec![
+        Move::debug_new_basic("h8", "g8"),
+        Move::debug_new_basic("h8", "g7"),
+        Move::debug_new_basic("h8", "h7"),
+    ];
+
+    let received_moves = position.get_legal_moves_for_tile_position(TilePosition::from_tile_str("h8").unwrap());
+
+    assert!(compare_moves(&desired_moves, &received_moves)); 
+}
