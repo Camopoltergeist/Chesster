aa8a1c283176192cee958a7bdb03050e27a62fec
Camopoltergeist <camopoltergeist@outlook.com>
2025-02-11 13:47:36 +0200

    Add knight movement without collision test a1

---
+++ src/tests/knight_movement.rs
@@ -0,0 +1,18 @@
+use crate::board::position::Position;
+use crate::board::{moove::Move, tile_position::TilePosition};
+
+#[test]
+fn knight_movement_without_collision_a1() {
+    use crate::tests::compare_moves;
+
+    let position = Position::from_fen_str("8/8/8/8/8/8/8/N7 w - - 0 1").unwrap();
+
+    let desired_moves = vec![
+        Move::debug_new_basic("a1", "b3"),
+        Move::debug_new_basic("a1", "c2"),
+    ];
+
+    let received_moves = position.get_legal_moves_for_tile_position(TilePosition::from_tile_str("a1").unwrap());
+
+    assert!(compare_moves(&desired_moves, &received_moves));
+}
