eab3c747321731d3f7f88b65ef6e68eef97ec9bf
Camopoltergeist <camopoltergeist@outlook.com>
2025-02-11 13:58:19 +0200

    Add knight collision with friendly pieces test

--- src/tests/knight_movement.rs
+++ src/tests/knight_movement.rs
@@ -57,3 +57,26 @@ fn knight_movement_without_collision_d4() {
 
     assert!(compare_moves(&desired_moves, &received_moves));    
 }
+
+#[test]
+fn knight_collision_with_friendly_pieces() {
+    let position = Position::from_fen_str("8/8/2P5/5P2/3N4/1P3P2/8/8 w - - 0 1").unwrap();
+
+    let desired_moves = vec![
+        // North West
+        Move::debug_new_basic("d4", "b5"),
+
+        // North East
+        Move::debug_new_basic("d4", "e6"),
+
+        // South East
+        Move::debug_new_basic("d4", "e2"),
+
+        // South West
+        Move::debug_new_basic("d4", "c2"),
+    ];
+
+    let received_moves = position.get_legal_moves_for_tile_position(TilePosition::from_tile_str("d4").unwrap());
+
+    assert!(compare_moves(&desired_moves, &received_moves));
+}
