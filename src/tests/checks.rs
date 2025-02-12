a88e6aace1d7ec86b3e19056532e46a875936e24
Camopoltergeist <camopoltergeist@outlook.com>
2025-02-13 00:06:38 +0200

    Add can't move away while pinned test

--- src/tests/checks.rs
+++ src/tests/checks.rs
@@ -8,3 +8,12 @@ fn cant_leave_king_in_check() {
 
 	assert!(!position.is_legal_move(&illegal_move));
 }
+
+#[test]
+fn cant_move_away_while_pinned() {
+	let position = Position::from_fen_str("4r3/8/8/8/8/8/4B3/4K3 w - - 0 1").unwrap();
+
+	let illegal_move = Move::debug_new_basic("e2", "a6");
+
+	assert!(!position.is_legal_move(&illegal_move));
+}
