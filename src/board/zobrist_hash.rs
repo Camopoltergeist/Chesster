42aade88293187c36f801b65b3415e149cfab515
Camopoltergeist <camopoltergeist@outlook.com>
2025-02-28 03:32:18 +0200

    Implemented zobrist hash generation from position

--- src/board/zobrist_hash.rs
+++ src/board/zobrist_hash.rs
@@ -1,16 +1,81 @@
 use rand::{RngCore, SeedableRng};
 use rand_chacha::ChaCha20Rng;
 
+use crate::{piece::PieceType, player::Player, player_piece::PlayerPiece};
+
+use super::{moove::CastleSide, position::Position, tile_position::TilePosition};
+
 pub struct ZobristHash {
 	value: u64
 }
 
-static mut ROOK_NUMBERS: [u64; 64] = [0; 64];
-static mut KNIGHT_NUMBERS: [u64; 64] = [0; 64];
-static mut BISHOP_NUMBERS: [u64; 64] = [0; 64];
-static mut QUEEN_NUMBERS: [u64; 64] = [0; 64];
-static mut KING_NUMBERS: [u64; 64] = [0; 64];
-static mut PAWN_NUMBERS: [u64; 64] = [0; 64];
+impl ZobristHash {
+	pub fn from_position(position: &Position) -> Self {
+		let mut value = 0;
+
+		for bit_offset in 0..64 {
+			let tile_position = TilePosition::from_bit_offset(bit_offset);
+
+			let piece = position.get_piece(tile_position);
+
+			if piece.is_none() {
+				continue;
+			}
+
+			let piece = piece.unwrap();
+
+			value = value ^ get_zobrist_piece_number(piece, tile_position);
+		};
+
+		unsafe {
+			if position.get_castling_availability(Player::White, CastleSide::KingSide) {
+				value = value ^ WHITE_SHORT_CASTLE_NUMBER;
+			};
+
+			if position.get_castling_availability(Player::White, CastleSide::QueenSide) {
+				value = value ^ WHITE_LONG_CASTLE_NUMBER;
+			};
+
+			if position.get_castling_availability(Player::Black, CastleSide::KingSide) {
+				value = value ^ BLACK_SHORT_CASTLE_NUMBER;
+			};
+
+			if position.get_castling_availability(Player::Black, CastleSide::QueenSide) {
+				value = value ^ BLACK_LONG_CASTLE_NUMBER;
+			};
+
+			if position.current_player() == Player::Black {
+				value = value ^ BLACK_TO_MOVE;
+			};
+
+			if let Some(tile_pos) = position.en_passant_target.clone() {
+				value = value ^ EN_PASSANT_COLUMN_NUMBERS[tile_pos.column() as usize];
+			};
+		};
+
+		Self {
+			value
+		}
+	}
+
+	pub fn value(&self) -> u64 {
+		self.value
+	}
+}
+
+static mut WHITE_PAWN_NUMBERS: [u64; 64] = [0; 64];
+static mut WHITE_ROOK_NUMBERS: [u64; 64] = [0; 64];
+static mut WHITE_KNIGHT_NUMBERS: [u64; 64] = [0; 64];
+static mut WHITE_BISHOP_NUMBERS: [u64; 64] = [0; 64];
+static mut WHITE_QUEEN_NUMBERS: [u64; 64] = [0; 64];
+static mut WHITE_KING_NUMBERS: [u64; 64] = [0; 64];
+
+static mut BLACK_PAWN_NUMBERS: [u64; 64] = [0; 64];
+static mut BLACK_ROOK_NUMBERS: [u64; 64] = [0; 64];
+static mut BLACK_KNIGHT_NUMBERS: [u64; 64] = [0; 64];
+static mut BLACK_BISHOP_NUMBERS: [u64; 64] = [0; 64];
+static mut BLACK_QUEEN_NUMBERS: [u64; 64] = [0; 64];
+static mut BLACK_KING_NUMBERS: [u64; 64] = [0; 64];
 
 static mut WHITE_SHORT_CASTLE_NUMBER: u64 = 0;
 static mut WHITE_LONG_CASTLE_NUMBER: u64 = 0;
@@ -26,12 +91,19 @@ pub fn generate_zobrist_numbers() {
 	let mut rng = ChaCha20Rng::seed_from_u64(1377);
 
 	unsafe {
-		ROOK_NUMBERS = generate_numbers(&mut rng);
-		PAWN_NUMBERS = generate_numbers(&mut rng);
-		KNIGHT_NUMBERS = generate_numbers(&mut rng);
-		BISHOP_NUMBERS = generate_numbers(&mut rng);
-		QUEEN_NUMBERS = generate_numbers(&mut rng);
-		KING_NUMBERS = generate_numbers(&mut rng);
+		WHITE_PAWN_NUMBERS = generate_numbers(&mut rng);
+		WHITE_ROOK_NUMBERS = generate_numbers(&mut rng);
+		WHITE_KNIGHT_NUMBERS = generate_numbers(&mut rng);
+		WHITE_BISHOP_NUMBERS = generate_numbers(&mut rng);
+		WHITE_QUEEN_NUMBERS = generate_numbers(&mut rng);
+		WHITE_KING_NUMBERS = generate_numbers(&mut rng);
+
+		BLACK_PAWN_NUMBERS = generate_numbers(&mut rng);
+		BLACK_ROOK_NUMBERS = generate_numbers(&mut rng);
+		BLACK_KNIGHT_NUMBERS = generate_numbers(&mut rng);
+		BLACK_BISHOP_NUMBERS = generate_numbers(&mut rng);
+		BLACK_QUEEN_NUMBERS = generate_numbers(&mut rng);
+		BLACK_KING_NUMBERS = generate_numbers(&mut rng);
 
 		WHITE_SHORT_CASTLE_NUMBER = rng.next_u64();
 		WHITE_LONG_CASTLE_NUMBER = rng.next_u64();
@@ -63,4 +135,33 @@ fn generate_en_passant_numbers(rng: &mut ChaCha20Rng) -> [u64; 8] {
 	}
 
 	return arr;
+}
+
+fn get_zobrist_piece_number(piece: PlayerPiece, tile_position: TilePosition) -> u64 {
+	let i = tile_position.bit_offset() as usize;
+
+	unsafe {
+		match piece.player() {
+			Player::White => {
+				match piece.piece() {
+					PieceType::Pawn => WHITE_PAWN_NUMBERS[i],
+					PieceType::Rook => WHITE_ROOK_NUMBERS[i],
+					PieceType::Knight => WHITE_KNIGHT_NUMBERS[i],
+					PieceType::Bishop => WHITE_BISHOP_NUMBERS[i],
+					PieceType::Queen => WHITE_QUEEN_NUMBERS[i],
+					PieceType::King => WHITE_KING_NUMBERS[i]
+				}
+			},
+			Player::Black => {
+				match piece.piece() {
+					PieceType::Pawn => BLACK_PAWN_NUMBERS[i],
+					PieceType::Rook => BLACK_ROOK_NUMBERS[i],
+					PieceType::Knight => BLACK_KNIGHT_NUMBERS[i],
+					PieceType::Bishop => BLACK_BISHOP_NUMBERS[i],
+					PieceType::Queen => BLACK_QUEEN_NUMBERS[i],
+					PieceType::King => BLACK_KING_NUMBERS[i]
+				}
+			}
+		}
+	}
 }
