1afaf9a5bc6a95e753b7134a98b7dc2ec038ab09
Camopoltergeist <camopoltergeist@outlook.com>
2025-03-16 17:23:32 +0200

    Move docs

--- src/board/moove.rs
+++ src/board/moove.rs
@@ -1,9 +1,11 @@
-//! Implementations for basic, castling, en passant and romoting moves.
+//! Implementations for basic, castling, en passant and promoting moves.
+//! The name is "moove" as `move` is a keyword in Rust.
 
 use crate::{player::Player, player_piece::PlayerPiece};
 
 use super::tile_position::TilePosition;
 
+/// Enum representing any possible chess moves.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Move {
     Basic(BasicMove),
@@ -13,23 +15,41 @@ pub enum Move {
 }
 
 impl Move {
-    /// Quickly construct BasicMove. Used for testing.
+    /// Quickly construct [`Self::Basic`] variant.
+    /// 
+    /// Not recommended to be used outside of testing as it is slow.
+    /// 
+    /// Both `from` and `to` parameters are chess tiles as `&str`.
     pub fn debug_new_basic(from: &str, to: &str) -> Move {
         Move::Basic(BasicMove::new(TilePosition::from_tile_str(from).unwrap(), TilePosition::from_tile_str(to).unwrap()))
     }
 
+    /// Construct [`Self::Castling`] variant for specified [`player`][Player] and [`side`][CastleSide].
     pub fn new_castling(player: Player, side: CastleSide) -> Move {
         Move::Castling(CastlingMove::new(player, side))
     }
 
+    /// Quickly construct new [`Self::EnPassant`] variant.
+    /// 
+    /// Not recommended to be used outside of testing as it is slow.
+    /// 
+    /// All `from`, `to` and `target_square` parameters are chess tiles as `&str`.  
+    /// Validity of `target_square` is not checked.
     pub fn debug_new_en_passant(from: &str, to: &str, target_square: &str) -> Move {
         Move::EnPassant(EnPassantMove::new(TilePosition::from_tile_str(from).unwrap(), TilePosition::from_tile_str(to).unwrap(), TilePosition::from_tile_str(target_square).unwrap()))
     }
 
+    /// Quickly construct new [`Self::Promoting`] variant.
+    /// 
+    /// Not recommended to be used outside of testing as it is slow.
+    /// 
+    /// `from` and `to` parameters are chess tiles as `&str`.  
+    /// [`promotion_piece`][PlayerPiece] is the piece this move promotes to. 
     pub fn debug_new_promoting(from: &str, to: &str, promotion_piece: PlayerPiece) -> Move {
         Move::Promoting(PromotingMove::new(TilePosition::from_tile_str(from).unwrap(), TilePosition::from_tile_str(to).unwrap(), promotion_piece))
     }
 
+    /// Get target [`TilePosition`] of the king piece for specified [`player`][Player] and [`side`][CastleSide].
     pub fn get_castling_target(player: Player, side: CastleSide) -> TilePosition {
         let rank = player.castling_rank();
         let column = side.castling_king_target_column();
@@ -37,6 +57,7 @@ impl Move {
         TilePosition::new(column, rank)
     }
 
+    /// Get debug [`String`] of current position.
     pub fn debug_string(&self) -> String {
         match self {
             Self::Basic(basic_move) => {
@@ -59,6 +80,7 @@ impl Move {
         }
     }
 
+    /// Get [`TilePosition`] of the move's starting position.
     pub fn from_position(&self) -> TilePosition {
         match self {
             Self::Basic(basic_move) => basic_move.from_position(),
@@ -68,6 +90,7 @@ impl Move {
         }
     }
 
+    /// Get [`TilePosition`] of the move's landing position.
     pub fn to_position(&self) -> TilePosition {
         match self {
             Self::Basic(basic_move) => basic_move.to_position(),
@@ -102,6 +125,7 @@ impl From<PromotingMove> for Move {
     }
 }
 
+/// Represents basic chess moves including captures.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct BasicMove {
     from: TilePosition,
@@ -116,10 +140,12 @@ impl BasicMove {
         }
     }
 
+    /// See [`Move::from_position`].
     pub fn from_position(&self) -> TilePosition {
         self.from
     }
 
+    /// See [`Move::to_position`].
     pub fn to_position(&self) -> TilePosition {
         self.to
     }
@@ -137,6 +163,7 @@ impl From<PromotingMove> for BasicMove {
     }
 }
 
+/// Represents castling chess move.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct CastlingMove {
     player: Player,
@@ -151,10 +178,12 @@ impl CastlingMove {
         }
     }
 
+    /// See [`Move::from_position`].
     pub fn from_position(&self) -> TilePosition {
         self.player.castling_king_starting_position()
     }
 
+    /// See [`Move::to_position`].
     pub fn to_position(&self) -> TilePosition {
         let column = self.side.castling_king_target_column();
         let rank = self.player.castling_rank();
@@ -162,6 +191,7 @@ impl CastlingMove {
         TilePosition::new(column, rank)
     }
 
+    /// Starting [`TilePosition`] of the rook
     pub fn rook_from_position(&self) -> TilePosition {
         let column = self.side.castling_rook_starting_column();
         let rank = self.player.castling_rank();
@@ -169,6 +199,7 @@ impl CastlingMove {
         TilePosition::new(column, rank)
     }
 
+    /// Landing [`TilePosition`] of the rook
     pub fn rook_to_position(&self) -> TilePosition {
         let column = self.side.castling_rook_target_column();
         let rank = self.player.castling_rank();
@@ -176,18 +207,22 @@ impl CastlingMove {
         TilePosition::new(column, rank)
     }
 
+    /// Get the king's movement as [`BasicMove`]
     pub fn king_basic_move(&self) -> BasicMove {
         BasicMove::new(self.from_position(), self.to_position())
     }
 
+    /// Get the rook's movement as [`BasicMove`]
     pub fn rook_basic_move(&self) -> BasicMove {
         BasicMove::new(self.rook_from_position(), self.rook_to_position())
     }
 
+    /// [`Player`] that made the move
     pub fn player(&self) -> Player {
         self.player
     }
 
+    /// [`CastleSide`] of the move
     pub fn side(&self) -> CastleSide {
         self.side.clone()
     }
@@ -222,6 +257,7 @@ impl CastleSide {
     }
 }
 
+/// Represents en passant chess move.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct EnPassantMove {
     from: TilePosition,
@@ -238,19 +274,23 @@ impl EnPassantMove {
         }
     }
 
+    /// See [`Move::from_position`].
     pub fn from_position(&self) -> TilePosition {
         self.from
     }
 
+    /// See [`Move::to_position`].
     pub fn to_position(&self) -> TilePosition {
         self.to
     }
 
+    /// Tile which is the target of en passant capture.
     pub fn captured_tile(&self) -> TilePosition {
         self.captured_tile
     }
 }
 
+/// Represents a promoting chess move.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct PromotingMove {
     from: TilePosition,
@@ -267,14 +307,17 @@ impl PromotingMove {
         }
     }
 
+    /// See [`Move::from_position`].
     pub fn from_position(&self) -> TilePosition {
         self.from
     }
 
+    /// See [`Move::to_position`].
     pub fn to_position(&self) -> TilePosition {
         self.to
     }
 
+    /// [`PlayerPiece`] which this move promotes to.
     pub fn promotion_piece(&self) -> PlayerPiece {
         self.promotion_piece
     }
